a423d898eb891a98e5e1941e0a88f7dcda36ccfc
pedrojscript <pedrofigueiredoads@gmail.com>
2020-10-15 20:58:00 -0400

    #4

---
+++ src/database/fakedata.js
@@ -0,0 +1,37 @@
+module.exports = [
+  {
+    id:1,
+    lat: "-15.5963564",
+    lng: "-56.0935773",
+    name: "Lar dos meninas",
+    description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+    images: [
+      "https://images.unsplash.com/photo-1600711724779-c039d7410ceb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1563465814437-b1a057a461ed?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas das 8h as 18h",
+    open_on_weekends: "1",
+
+  },
+
+
+
+
+  {
+    id:2,
+    lat: "-15.5963565",
+    lng: "-56.0945773",
+    name: "Lar das meninas",
+    description: "lorem",
+    images: [
+      "https://images.unsplash.com/photo-1600711724779-c039d7410ceb?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+      "https://images.unsplash.com/photo-1563465814437-b1a057a461ed?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+    ],
+    instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+    opening_hours: "Horário de visitas das 8h as 18h",
+    open_on_weekends: "0",
+  }
+]
